1c8df2e903d80e1713e91da0a2745d9430ca73b4
vasilismouchakismamaai <vasilis.mouchakis@themama.ai>
2023-04-04 13:01:30 +0300

    add test

--- numeric_vision/app.py
+++ numeric_vision/app.py
@@ -1,6 +1,7 @@
 from numeric_vision.utils import word_to_num
 
 if __name__ == "__main__":
-    text = "five million, minus three, point four"
-    match = word_to_num(text)
+    text = "minus point two"
+    match = word_to_num(text)[0]
+    print(type(match))
     print(f"the match is: {match}")
---
+++ test/test_word_to_num.py
@@ -0,0 +1,27 @@
+import unittest
+from numeric_vision.utils import word_to_num
+
+
+class TestWordToNum(unittest.TestCase):
+    def test_positive_numbers(self):
+        self.assertEqual(word_to_num("five")[0], 5)
+        self.assertEqual(word_to_num("zero")[0], 0)
+        self.assertEqual(word_to_num("one million")[0], 1000000)
+        self.assertEqual(word_to_num("one billion")[0], 1000000000)
+        self.assertEqual(word_to_num("five billion two thousand eight hundred nineteen")[0], 5000002819)
+        self.assertEqual(word_to_num("two hundred sixty five")[0], 265)
+        self.assertEqual(word_to_num("ten")[0], 10)
+
+    def test_negative_numbers(self):
+        self.assertEqual(word_to_num("minus two")[0], -2)
+        self.assertEqual(word_to_num("minus point two")[0], -0.2)
+        self.assertEqual(word_to_num("minus one million")[0], -1000000)
+        self.assertEqual(word_to_num("minus two billion")[0], -2000000000)
+
+    def test_decimals(self):
+        self.assertEqual(word_to_num("point six")[0], 0.6)
+        self.assertEqual(word_to_num("minus point six")[0], -0.6)
+        self.assertEqual(word_to_num("point zero seven")[0], 0.07)
+
+if __name__ == '__main__':
+    unittest.main()
