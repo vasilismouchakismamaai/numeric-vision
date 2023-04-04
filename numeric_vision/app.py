2f5c2fa825f85fbb53389ac133f59464767daee5
vasilismouchakismamaai <vasilis.mouchakis@themama.ai>
2023-04-04 13:31:38 +0300

    add arg parser

--- numeric_vision/app.py
+++ numeric_vision/app.py
@@ -1,7 +1,16 @@
 from numeric_vision.utils import word_to_num
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--text", type=str, required=True, help="sentence to extract numbers")
+
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    text = "minus point two"
-    match = word_to_num(text)[0]
-    print(type(match))
+    args = parse_args()
+    text = args.text
+    match = word_to_num(text)
     print(f"the match is: {match}")
